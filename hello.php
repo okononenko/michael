f86b41192cdcd7a6f74906546f17f47fc4f975f3
okononenko <okononenko@speroteck.com>
2013-07-10 20:20:44 +0300

    ver 2.0

--- hello.php
+++ hello.php
@@ -1,3 +1,7 @@
+<?php if ()?>
+
+
+
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
 <script type="text/javascript">
     jQuery(document).ready(function(){
