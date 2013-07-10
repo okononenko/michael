464d6ab48315037f6b18078efb3c3ca16dc109d3
okononenko <okononenko@speroteck.com>
2013-07-10 18:59:37 +0300

    add server rules

---
+++ hello.php
@@ -0,0 +1,20 @@
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+<script type="text/javascript">
+    jQuery(document).ready(function(){
+        if(jQuery(".form").val() == "michael") {
+            //alert ('Hello, Michael Jackson');
+            jQuery("par1").html("Hello, Michael Jackson");
+        }
+        else {
+            //alert ('you are not Michael');
+            jQuery("par1").html("Hello, Michael Jackson");
+        }
+    });
+</script>
+
+
+<?php
+/**
+<?php echo $this->__('Hello World'); ?>
+ */
+?>
