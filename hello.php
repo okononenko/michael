19baf0ed3e398915969c868e629e546a96222af2
okononenko <okononenko@speroteck.com>
2013-07-11 19:40:52 +0300

    jquery.ajax plus php

--- hello.php
+++ hello.php
@@ -1,4 +1,10 @@
 <?php
-if ( isset ($_POST ['user']) and $_POST ['user'] == 'michael') {echo ("<p>Hello, Michael</p>");}
-else {echo "<p>error</p>";}
+if (isset($_POST['user']) && ($_POST['user'] == 'michael')){
+    echo "Hello, ".$_POST['user'].
+        "<br/>your age is: ".$_POST['age'].
+        "<br/>your sex is: ".$_POST['sex'].
+        "<br/>your number is: ".$_POST['num'];
+} else {
+    echo "i dont care who you are";
+}
 
